ff728808f6e0567f000483faafacca505429e7ba
Викуша <viktoriya_kudash@mail.ru>
2018-12-04 15:16:28 +0300

    Add 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool photo = true;
+            int srok = 9;
+            bool usePhoto = false;
+
+            if (photo == true)
+            {
+                if (srok <= 3)
+                {
+                    if (photo != usePhoto)
+                    {
+                        Console.WriteLine("Оплачивайте");
+                    }
+                
+                }
+                else
+                {
+                    Console.WriteLine("идите в фотокомнату");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
