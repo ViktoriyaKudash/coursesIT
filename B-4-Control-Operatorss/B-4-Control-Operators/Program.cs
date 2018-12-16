0a17814db3f19c92a99a741d1ea831bf612cfbaa
Виктория <viktoriya_kudash@mail.ru>
2018-12-16 16:09:14 +0300

    Add 4

---
+++ B-4-Control-Operatorss/B-4-Control-Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Lesson_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lesson.LessonExampleWhile();
+
+            //Practice.B4_P5_25_Switch_DayOfWeek();
+            //Practice.B4_P6_25_Switch_GameNavigation();
+            Console.ReadLine();
+        }
+     }
+}
